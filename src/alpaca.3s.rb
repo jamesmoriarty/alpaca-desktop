104cfee5dee36cd391453ccb364065ad0ccf90a8
James Moriarty <james.moriarty@anz.com>
2019-10-16 11:57:22 +1100

    feature: uninstall script.

--- src/alpaca.3s.rb
+++ src/alpaca.3s.rb
@@ -4,13 +4,14 @@ require 'base64'
 
 # Configuration
 
-APP_PATH     = File.join(File.expand_path(File.dirname(File.dirname(__FILE__))), "MacOS")
-ALPACA_PATH  = File.join(APP_PATH, "/bin/alpaca")
-INSTALL_PATH = File.join(APP_PATH, "/bin/install")
-LOG_PATH     = File.join(Dir.home, ".alpaca.log")
-PLIST_NAME   = "com.github.alpaca"
-PLIST_PATH   = File.expand_path(File.join(Dir.home, "/Library/LaunchAgents/#{PLIST_NAME}.plist"))
-ICON_BASE64  = Base64.strict_encode64(IO.read(File.join(APP_PATH, "/img/icon.png"))).chomp
+APP_PATH       = File.join(File.expand_path(File.dirname(File.dirname(__FILE__))), "MacOS")
+ALPACA_PATH    = File.join(APP_PATH, "/bin/alpaca")
+INSTALL_PATH   = File.join(APP_PATH, "/bin/install")
+UNINSTALL_PATH = File.join(APP_PATH, "/bin/uninstall")
+LOG_PATH       = File.join(Dir.home, ".alpaca.log")
+PLIST_NAME     = "com.github.alpaca"
+PLIST_PATH     = File.expand_path(File.join(Dir.home, "/Library/LaunchAgents/#{PLIST_NAME}.plist"))
+ICON_BASE64    = Base64.strict_encode64(IO.read(File.join(APP_PATH, "/img/icon.png"))).chomp
 
 def running?(plist_name)
   return `launchctl list #{plist_name}`[/"PID" = (\d+)/, 1]
@@ -31,7 +32,7 @@ elsif File.exists? PLIST_PATH
 else
   puts "● | color=red templateImage=#{ICON_BASE64}"
   puts "---"
-  puts "Install Daemon | terminal=false bash=#{INSTALL_PATH} args=#{PLIST_NAME}__#{PLIST_PATH}__#{ALPACA_PATH}__#{LOG_PATH}"
+  puts "Install | terminal=false bash=#{INSTALL_PATH} args=#{PLIST_NAME}__#{PLIST_PATH}__#{ALPACA_PATH}__#{LOG_PATH}"
 end
 
   # Log tail
@@ -41,3 +42,6 @@ if File.exists? LOG_PATH
   puts "---"
   puts `tail -n 3 #{LOG_PATH}`
 end
+
+puts "---"
+puts "Uninstall | terminal=false bash=#{UNINSTALL_PATH} args=#{PLIST_NAME}__#{PLIST_PATH}__#{ALPACA_PATH}__#{LOG_PATH}"
